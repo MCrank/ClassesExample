ab598c641efe54acad578fbc46c44ebb7bab75c5
MCrank <marco_mcse@hotmail.com>
2019-03-02 13:54:10 -0600

    Code along classes

---
+++ ClassesExample/Candy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesExample
+{
+    class Candy
+    {
+        public string Flavor { get; set; }
+        public int Rating { get; set; } = 1; // <-- Property Initailizer.  Can set default value. Do not need to set in CTOR
+        public string Review { get; set; }
+        public string Name { get; set; }
+        public CandyType Type { get; set; }
+
+        const int MinRatingValue = 1;
+        const int MaxRatingValue = 10;
+
+        public Candy(string flavor, string name, CandyType type)
+        {
+            Flavor = flavor;
+            Name = name;
+            Type = type;
+        }
+
+        // Methods
+        public void SetRating(int userRating)
+        {
+            // Needs to be between 1 & 10
+            if (userRating >= MinRatingValue && userRating <= MaxRatingValue)
+            {
+                Rating = userRating;
+            }
+            else
+            {
+                throw new Exception($"You are dumb. Ratings are {MinRatingValue}-{MaxRatingValue}. You moron");
+            }
+        }
+
+        public void SetRating(int userRating, string review)
+        {
+            SetRating(userRating);
+            Review = review;
+        }
+
+        // Expression Body Members
+        // Akin to the JS Fat Arrow function - Typically used for small chunks of code, WOuld not use for more complicated function/Method
+        public void SetRating(bool likedIt) => Rating = likedIt ? 10 : 1;
+
+        public override string ToString()
+        {
+            return $"This is {Type} {Name}, with a(n) {Flavor} flavor, rated {Rating}";
+        }
+
+    }
+
+    
+
+    enum CandyType
+    {
+        Chocolate,
+        HardCandy,
+        Caramel,
+        Sour,
+        Stretchy,
+        Nut
+    }
+}
--- ClassesExample/Program.cs
+++ ClassesExample/Program.cs
@@ -2,11 +2,43 @@
 
 namespace ClassesExample
 {
+    // Classes have Fields (Internal), Properties (External), Methods (Actions that I can take)
+    // Classes are implicitely Internal (Not accessibly outside teh project)
     class Program
     {
+        #region Fields
+        string _myData = "This is just for me to use";
+
+        #endregion
+
+        #region Property
+        // Properties and Fields - two ways to store information
+        // Property used ot expose 'state' external to class
+        public int Number { get; set; }
+
+        public void DoTheThing()
+        {
+            Number = 1;
+            var x = Number;
+        }
+        #endregion
+
+        // Access Modifiers
+        // Implicit modifiers inside a class are private
+
+        // Static Accessability modifier - Do not need to Instantiate the Class
+        // Cannot call non static methods
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var taffy = new Candy("Orange", "Saltwater Taffy", CandyType.Stretchy);
+
+            taffy.SetRating(7, "This is guud!");
+            
+            Console.WriteLine(taffy);
+            Console.ReadLine();
+
         }
     }
 }
+
+// Expression Body Members
